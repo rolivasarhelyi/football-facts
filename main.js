cfce8ba422068adb91b4b046d667d78b29033753
Roland Vásárhelyi <75977095+rolivasarhelyi@users.noreply.github.com>
2025-05-11 12:11:09 +0200

    Added some facts.

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ function generateFact() {
     'The fastest red card in football history was just 2 seconds after kickoff.',
     'Goalkeepers were allowed to pick up back-passes until 1992.',
     'The World Cup trophy has been stolen — and found — twice!',
-    'Levent is a noob at Fifa',
-    'There is a new sheriff in town called Roland Vasarhelyi',
+    'Levent is a pro at FC24 corners.',
+    'There is a new sheriff in town called Levent Suzgun',
+    'Roland can win against Older Suzgun.',
   ];
 
   const randomIndex = Math.floor(Math.random() * facts.length);
